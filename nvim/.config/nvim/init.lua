e9a4e249813846ee3db96f7f77660f8ddecf9eab
Curtis Onuczko <ositruc@gmail.com>
2024-01-02 07:07:20 -0700

    Adding key map for File E[x]plorer

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -301,6 +301,8 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+vim.keymap.set({'n'}, '<leader>x', vim.cmd.Ex, { desc = 'File E[x]plorer' })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
